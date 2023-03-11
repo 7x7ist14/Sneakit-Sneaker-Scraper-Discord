cf09092537326938b0fddd15579d1a877fc7908f
jakob.aio <103487648+7x7ist14@users.noreply.github.com>
2023-03-11 17:26:21 +0100

    Update config.py

--- Sneakit/config.py
+++ Sneakit/config.py
@@ -1,3 +1,10 @@
+
+#Input your discord bot token here:
 TOKEN = ''
 
-CHANNEL_NAME = ''
+#Input the name of the discord channel here (channel not server!):
+CHANNEL_NAME = 'scraper'
+
+#if you want to change the command you can do it here:
+COMMAND_PREFIX = "$sneakit"
+# $sneakit is the standart command but you can change it how you want to. for example: %scrape
